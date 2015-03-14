8f2d89630d1f77984e3daaf049d334377a609beb
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-03-13 19:06:27 -0600

    Fix App Route

--- app/routes/handlers/App.js
+++ app/routes/handlers/App.js
@@ -1,12 +1,18 @@
 var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+// var AddRequirement = require('../../components/AddRequirement');
 
 
 var App = React.createClass({
   render: function () {
     return (
-      <h1>App</h1>
+      <div>
+        <h1>App</h1>
+        <RouteHandler />
+      </div>
     );
   }
 });
+
 
 module.exports = App;
